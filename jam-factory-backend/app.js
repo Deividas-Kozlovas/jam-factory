bc102e1f52a9b712ab3a28d1eae48396f106295c
Deividas Kozlovas <kozlovas.deividas@gmail.com>
2025-01-31 12:59:39 +0200

    setup: env,morgan and express routes

--- jam-factory-backend/app.js
+++ jam-factory-backend/app.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+const jamRouter = require("./routes/jamRoutes");
+
+app.use(express.json());
+
+app.use("/api/v1/jam", jamRouter);
 
 module.exports = app;
