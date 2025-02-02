d665e453b1d45ebfcc8eef6f3ebad6de2c8406ab
Deividas Kozlovas <kozlovas.deividas@gmail.com>
2025-02-02 16:22:54 +0200

    add: middleware for jam input and id validation

---
+++ jam-factory-backend/middlewares/jamMiddleware.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const Jam = require("../models/jamModel");
+
+exports.validateJam = (req, res, next) => {
+  const {
+    name,
+    fruitType,
+    batchSize,
+    sugarAmount,
+    productionDate,
+    expirationDate,
+  } = req.body;
+
+  if (
+    !name ||
+    !fruitType ||
+    !batchSize ||
+    !sugarAmount ||
+    !productionDate ||
+    !expirationDate
+  ) {
+    return res.status(400).json({
+      status: "Failed",
+      message: "Please fill all required fields",
+    });
+  }
+
+  next();
+};
+
+exports.checkJamID = async (req, res, next, val) => {
+  if (mongoose.Types.ObjectId.isValid(val)) {
+    return res.status(400).json({
+      status: "Failed",
+      message: "Jam not found",
+    });
+  }
+  next();
+};
--- jam-factory-backend/routes/jamRoutes.js
+++ jam-factory-backend/routes/jamRoutes.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const jamController = require("../controllers/jamController");
+const jamMiddleware = require("../middlewares/jamMiddleware");
 
 const router = express.Router();
 
-router.route("/").get(jamController.getAllJams).post(jamController.createJam);
+router
+  .route("/")
+  .get(jamController.getAllJams)
+  .post(jamMiddleware.validateJam, jamController.createJam);
 
-router.route("/:id").get(jamController.getJam);
+router.route("/:id").get(jamMiddleware.checkJamID, jamController.getJam);
 
 module.exports = router;
