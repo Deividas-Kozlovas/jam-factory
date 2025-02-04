5011fe209d1808c26571d3ad20c10e991009896b
Deividas Kozlovas <kozlovas.deividas@gmail.com>
2025-02-04 16:26:08 +0200

    add: login user, with password validation and tocken generation

--- jam-factory-backend/controllers/authConteller.js
+++ jam-factory-backend/controllers/authConteller.js
@@ -32,3 +32,52 @@ exports.singup = async (req, res) => {
     });
   }
 };
+
+// Function to generate a JSON Web Token (JWT) for a user
+const singToken = (id) => {
+  // Create a JWT token with the user's ID, using the JWT_SECRET and JWT_EXPIRES_IN from environment variables
+  return jwt.sign({ id: id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN, // Token expiration time (from env variable)
+  });
+};
+
+// Controller function to handle user login
+exports.login = async (req, res) => {
+  try {
+    // Destructure email and password from the request body
+    const { email, password } = req.body;
+
+    // If email or password is not provided, throw an error
+    if (!email || !password) {
+      throw new Error("Please provide email and password");
+    }
+
+    // Query the User collection to find a user with the provided email
+    // The password field is selected manually here, even though it's hidden by default in the model
+    const user = await User.findOne({ email }).select("+password");
+
+    // If user is not found or password does not match, throw an error
+    if (!user || !(await user.correctPassword(password, user.password))) {
+      throw new Error("Incorrect email or password");
+    }
+
+    // Generate a JWT token for the user
+    const token = singToken(user.id);
+
+    // Send a response with the user data (excluding the password) and the generated token
+    res.status(201).json({
+      data: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+      },
+      token, // Include the generated token in the response
+    });
+  } catch (err) {
+    // If any error occurs, send a 400 status with the error message
+    res.status(400).json({
+      response: "Failed",
+      message: err.message, // Return the error message to the client
+    });
+  }
+};
--- jam-factory-backend/routes/userRoutes.js
+++ jam-factory-backend/routes/userRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const authController = require("../controllers/authConteller");
 
 router.post("/singup", authController.singup);
+router.post("/login", authController.login);
 
 module.exports = router;
